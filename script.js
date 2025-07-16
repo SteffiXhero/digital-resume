f93fff3ac9d17e32d6ade4f9bf2b794240cae5dc
Barwar Bakhtyar <161226405+SteffiXhero@users.noreply.github.com>
2025-07-16 21:33:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,13 +3,9 @@ document.getElementById('contactForm').addEventListener('submit', function (e) {
 
   const messageSentLabel = document.getElementById('messageSent');
 
-  // Show message sent label with fade-in effect
   messageSentLabel.classList.add('visible');
-
-  // Clear form inputs
   this.reset();
 
-  // Hide the message after 3 seconds
   setTimeout(() => {
     messageSentLabel.classList.remove('visible');
   }, 3000);
